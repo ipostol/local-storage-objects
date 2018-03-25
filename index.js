c35acf30ebbc4637ba9d2149033d69bfbdda6e18
Ivan <iqsk81ad@gmail.com>
2018-03-25 19:47:09 +0300

    #presave

---
+++ index.js
@@ -0,0 +1,40 @@
+const LS = {
+
+  getItem(key) {
+
+    const item = localStorage.getItem(key);
+
+    try {
+      return JSON.parse(item);
+    } catch (e) {
+      return item;
+    }
+
+  }
+
+  setItem(key, value) {
+
+    try {
+      if (typeof value === 'string') {
+        localStorage.setItem(key, value);
+      } else {
+        localStorage.setItem(key, JSON.stringify(value));
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+
+  }
+
+  clearKey(key) {
+    return localStorage.setItem(key, undefined);
+  }
+
+  clear() {
+    return localStorage.clear();
+  }
+
+}
+
+export default LS;
